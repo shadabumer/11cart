62c798883cc74cdb2b2544d0197f1f09e6874ff6
Shadab Umer <shadabum3r@gmail.com>
2019-12-05 14:58:58 +0530

    cart implemented

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,9 +1,92 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  amount: number;
+}
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
+  data: Product[] = [
+    {id: 0, name: 'onion', price: 50, amount: 1},
+    {id: 1, name: 'bread', price: 30, amount: 1},
+    {id: 2, name: 'chocolate', price: 100, amount: 1},
+  ];
+
+  private cart = [ 
+    {id: 0, name: 'onion', price: 50, amount: 1},
+    {id: 2, name: 'chocolate', price: 100, amount: 1},
+    {id: 1, name: 'bread', price: 30, amount: 1},
+   ];
+  private cartItemCount = new BehaviorSubject(0);
+
   constructor() { }
+
+  getProducts() {
+    return this.data;
+  }
+
+  getcart() {
+    return this.cart;
+  }
+
+  getCartItemCount() {
+    return this.cartItemCount;
+  }
+
+  addProduct(product) {
+    let added = false;
+    for (let p of this.cart) {
+      if (p.id === product.id) {
+        p.amount += 1;
+        added = true;
+        break;
+      }
+    }
+    if (!added) {
+      this.cart.push(product);
+    }
+    this.cartItemCount.next(this.cartItemCount.value + 1);
+  }
+
+  decreaseProduct(product) {
+    this.cart.forEach(function (p, index) {
+      if (p.id === product.id) {
+        p.amount -= 1;
+        if (p.amount == 0) {
+          this.cart.splice(index, 1);
+        }
+      }
+    })
+    // for (let [index, p] of this.cart.entries()) {
+    //   if (p.id === product.id) {
+    //     p.amount -= 1;
+    //     if (p.amount == 0) {
+    //       this.cart.splice(index, 1);
+    //     }
+    //   }
+    // }
+    this.cartItemCount.next(this.cartItemCount.value - 1);
+  }
+
+  removeProduct(product) {
+    this.cart.forEach(function(p, index) {
+      if (p.id === product.id) {
+        this.cartItemCount.next(this.cartItemCount.value - p.amount);
+        this.cart.splice(index, 1);
+      }
+    })
+    // for (let [index, p] of this.cart.entries()) {
+    //   if (p.id === product.id) {
+    //     this.cartItemCount.next(this.cartItemCount.value - p.amount);
+    //     this.cart.splice(index, 1);
+    //   }
+    // }
+  }
+
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+import { Product, CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
+  cart: Product[] = [];
 
-  constructor() {}
+  constructor(private cartService: CartService, private alertCtrl: AlertController) { }
+
+  ngOnInit() {
+    this.cart = this.cartService.getcart();
+  }
+
+  decreaseCartItem(product) {
+    this.cartService.decreaseProduct(product);
+  }
+ 
+  increaseCartItem(product) {
+    this.cartService.addProduct(product);
+  }
+ 
+  removeCartItem(product) {
+    this.cartService.removeProduct(product);
+  }
+
+  getTotal() {
+    return this.cart.reduce((i, j) => i + j.price * j.amount, 0);
+  }
+
+  async checkout() {
+    // Perfom PayPal or Stripe checkout process
+ 
+    let alert = await this.alertCtrl.create({
+      header: 'Thanks for your Order!',
+      message: 'We will deliver your food as soon as possible',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
 
 }
