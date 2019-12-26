599e3917aa2ca6cc4380fe8f109ee7c849d05846
Shadab <shadabum3r@gmail.com>
2019-12-26 07:57:57 +0530

    authentication successful

--- src/app/pages/public/login/login.page.ts
+++ src/app/pages/public/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
+import { UsersService } from 'src/app/shared/users.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  loginForm: FormGroup;
+  isSubmitted: boolean = false;
 
-  constructor() { }
+  constructor(public user: UsersService) { }
 
   ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    });
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.loginForm.controls; }
+
+  onSubmit() {
+    this.isSubmitted = true;
+    if (this.loginForm.invalid) return;
+
+    const user = this.user.login(this.f.email.value, this.f.password.value);
+    console.log('user logged in:', user);
+
+    this.loginForm.reset();
+    this.isSubmitted = false;
   }
 
 }
